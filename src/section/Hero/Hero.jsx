93d60eefdbbebde2d130b521810356f0637b5587
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-07-04 16:30:49 +0530

    dark images added

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -1,14 +1,21 @@
 import Style from './HeroStyle.module.css';
 import Heroimg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg';
-import twitterIcon from '../../assets/twitter-light.svg';
-import githubIcon from '../../assets/github-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import gitghubLight from '../../assets/github-light.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
 import { useTheme } from '../../common/ThemeContext';
 const Hero = () => {
 const {theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? gitghubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
   return (
     <section id='hero' className='Style.container'>
       <div className='Style.colorModeContainer'>
